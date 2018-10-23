1962a2b5e87d6f4489c4db2867e4c6d12b66222f
DonNinja <yngvileo@gmail.com>
2018-10-23 14:52:54 +0000

    Vann í fyrstu síðunni

--- VEFVerkefni/master.Master.designer.cs
+++ VEFVerkefni/master.Master.designer.cs
@@ -31,12 +31,12 @@ namespace VEFVerkefni {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
-        /// Changedtext control.
+        /// contentHolder control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder Changedtext;
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder contentHolder;
     }
 }
